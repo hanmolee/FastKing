1180871c97984b17618c444240f9189240c45bae
hanmolee <dlgksah@gmail.com>
2017-12-20 20:57:22 +0900

    Apollo Client로 GraphQL 통신 환경 설정

--- app/src/main/java/com/fastking/app/fastking/MainActivity.kt
+++ app/src/main/java/com/fastking/app/fastking/MainActivity.kt
@@ -2,6 +2,12 @@ package com.fastking.app.fastking
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import java.io.IOException
+import com.apollographql.apollo.ApolloClient
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +16,44 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        var user_id : String = input_id.text.toString()
+        var user_password : String = input_pwd.text.toString()
+
+        btn_login.setOnClickListener { login(user_id, user_password) }
+
+
+    }
+
+    /*private fun userLoginRequest() {
+
+        var Url : String = ""
+
+        val okHttpClient : OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build()
+
+
+        val apolloClient : ApolloClient = ApolloClient.builder()
+                .serverUrl(Url)
+                .okHttpClient(okHttpClient)
+                .normalizedCache(normalizedCacheFactory, cacheKeyResolver)
+                .build()
+
+    }*/
+
+
+    private fun login(user_id: String, user_password: String) {
+
+        try {
+            //userLoginRequest()
+        }
+        catch (e : IOException){
+
+        }
+
+        when(user_id){
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/fastking/app/fastking/activity/TestActivity.kt
@@ -0,0 +1,18 @@
+package com.fastking.app.fastking.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.fastking.app.fastking.R
+
+/**
+ * Created by mac on 2017. 12. 20..
+ */
+class TestActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+
+    }
+}
